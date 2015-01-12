7db0396a59d2040a901f82a51986297654551f01
Mark Janssen -- Sig-I/O Automatisering <mark@sig-io.nl>
2015-01-12 13:14:09 +0100

    Improve documentation

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,25 +1,25 @@
 /*
 -------------How to wire Arduino to Smart meter using RJ11 cable-------------
 Note: make sure to place a small resistor between between 5V & ground. Current from an Arduino output pin is 40mA, while 30mA is allowed on the P1 port.
-Yellow to pin 9
-Red to ground
-Black to pin  4
+Yellow to pin 9 (TX on meter, RX on arduino)
+Red to ground	(Yeah... illogical)
+Black to pin 4 or 5V (5V, also counter-intuitive)
 
 -------------Example P1 telegram----------------------------
 /ISk5\2ME382-1003
 
 0-0:96.1.1(4B414C37303035303739393336333132)
-1-0:1.8.1(00053.950*kWh)
-1-0:1.8.2(00081.586*kWh)
-1-0:2.8.1(00003.303*kWh)
-1-0:2.8.2(00009.299*kWh)
-0-0:96.14.0(0002)
-1-0:1.7.0(0000.03*kW)
+1-0:1.8.1(00053.950*kWh)	-> Used Low Tariff
+1-0:1.8.2(00081.586*kWh)	-> Used High Tariff
+1-0:2.8.1(00003.303*kWh)	-> Supplied Low Tariff
+1-0:2.8.2(00009.299*kWh)	-> Supplied High Tariff
+0-0:96.14.0(0002)		-> Current Tariff
+1-0:1.7.0(0000.03*kW)		-> Current Usage
 1-0:2.7.0(0000.00*kW)
-0-0:17.0.0(0999.00*kW)
-0-0:96.3.10(1)
-0-0:96.13.1()
-0-0:96.13.0()
+0-0:17.0.0(0999.00*kW)		-> Limit / Useless
+0-0:96.3.10(1)			-> Vendor specific
+0-0:96.13.1()			-> Vendor specific
+0-0:96.13.0()			-> Vendor specific
 !
 */
 
@@ -28,6 +28,9 @@ Black to pin  4
 
 const int RTSpin =  4;
 int incomingByte = 0;
+char buffer[512];	// Overkill
+int counter=0;
+int timer=0;
 
 SoftwareSerial mySerial(9, 8, true); // RX, TX, inverted
 
@@ -38,13 +41,30 @@ void setup () {
 
 	pinMode(RTSpin, OUTPUT);
 	digitalWrite(RTSpin, HIGH);
+	Serial.print("Arduino Smart-Meter Relay/Translator v0.1\r\n\n" );
+
+	buffer[counter] = '\0';
+	timer=millis();
 }
 
-void loop () {
-	while (mySerial.available() > 0) {
-		incomingByte = mySerial.read();
-		incomingByte &= ~(1 << 7);  // forces 0th bit to be 0. all other bits left alone.
-	  
-		Serial.write(incomingByte);
+void loop ()
+{
+	// If we haven't seen any new characters for 2 seconds, we must be done,
+	// so print the buffer, as the next one will arrive in another 8 seconds
+	int timer2 = millis();
+	if ( (  timer2 > (timer+2000) ) || ( timer2 < timer) )
+	{
+		buffer[counter++] = '\0';
+		Serial.print(buffer);
+		counter = 0;
+		buffer[counter] = '\0';
+		timer=millis();
+	}
+
+	while (mySerial.available() > 0)
+	{
+		// Pull 8th bit to zero
+		incomingByte = (mySerial.read() & B01111111);
+		buffer[counter++] = incomingByte;
 	}
 }
